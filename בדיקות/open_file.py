226830cad0f0d0f274ee0559139d48950a2cae82
effi_fertig <fefertig@icloud.com>
2025-06-19 10:41:02 +0300

    made some upgrades i think

--- בדיקות/open_file.py
+++ בדיקות/open_file.py
@@ -1,8 +1,12 @@
 def mkdir(name_of_file):
     File=open(name_of_file, 'w')
     return File
-file=mkdir('test.txt')
 
-def write_to_file(file_name,insert):
-    file_name.write(insert)
-write_to_file('test.txt','this is a test run \n hope it works')
+file=mkdir('test.txt')
+#it can both write to an existing file or make a knew one
+def write_in_file(file_name,insert):
+    with open(file_name,'a') as f:
+        f.write(insert)
+
+write_in_file('test.txt','this is a test run \nhope it works\n')
+write_in_file('test2.txt','it dose work!!')
